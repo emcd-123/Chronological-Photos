86ce33b33af97c5bdc86b8864e390ef3dfb5df1e
emcd-123 <emcd.unl@gmail.com>
2022-04-19 22:47:55 +0900

    Update chrono_photo.py

--- chrono_photo.py
+++ chrono_photo.py
@@ -1,1 +1,38 @@
 
+'''
+4/19/2022
+
+This program will organize all the photos in a folder in chronological order by when they were taken and rename them to their order
+'''
+import re
+import sys
+from os import rename
+from pathlib import Path
+from PIL import Image
+
+path = sys.argv[1]
+dates = []
+
+# finding the metadata for when the photos were taken
+for child in Path(path).iterdir():
+    file = "{}/{}".format(path,child.name)
+    with Image.open(file) as image:
+        exifdata = image.getexif()
+
+        data = exifdata.get(306)
+        dates.append([data,child.name])
+
+dates = sorted(dates)
+
+for i, j in enumerate(dates):
+    
+    t = re.search(r"\.\w+",j[1])
+    ext = t.group()
+    #filepath = path +'/'+ j[1]
+    new_name = str(i)+ext
+    #filepath.rename(path / new_name)
+    
+    rename(path +'/'+ j[1],path+'/'+ new_name)
+    
+print("Done")
+    
